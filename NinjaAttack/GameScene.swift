80e139275da4c0065e45b0d47ba42c32cffe2125
Adam Saxton <ams17a@acu.edu>
2019-03-29 09:05:01 -0500

    Added first sections

--- NinjaAttack/GameScene.swift
+++ NinjaAttack/GameScene.swift
@@ -29,5 +29,15 @@
 import SpriteKit
 
 class GameScene: SKScene {
+  // 1
+  let player = SKSpriteNode(imageNamed: "player")
   
+  override func didMove(to view: SKView) {
+    // 2
+    backgroundColor = SKColor.white
+    // 3
+    player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
+    // 4
+    addChild(player)
+  }
 }
